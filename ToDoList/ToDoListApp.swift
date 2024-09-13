75db489d60e1fac26ba6245a246bebbd827ae89c
Sipeng He <hsp8412@hotmail.com>
2024-09-13 00:03:19 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 何斯鹏 on 2024-09-13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
